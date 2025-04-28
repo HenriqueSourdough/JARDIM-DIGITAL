079fd84e5cd219a8557ed4dd291eccb4e49ce089
brutsabreu <bruhhabreu@gmail.com>
2025-04-28 09:18:16 -0300

    "atualizar"

--- JARDIM DIGITAL/Tela_Inicial.cs
+++ JARDIM DIGITAL/Tela_Inicial.cs
@@ -26,5 +26,15 @@ namespace JARDIM_DIGITAL
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
