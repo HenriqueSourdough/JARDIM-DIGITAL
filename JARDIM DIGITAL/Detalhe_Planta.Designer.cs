5392b96c3f8ab2be64818b10b1293c4b2623c5ce
HENRIQUE ROLDAO DE CAMPOS MELO <henriqueroldaodecamposmelo@gmail.com>
2025-04-30 08:31:43 -0300

    <atualizado>

--- JARDIM DIGITAL/Detalhe_Planta.Designer.cs
+++ JARDIM DIGITAL/Detalhe_Planta.Designer.cs
@@ -30,64 +30,88 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Detalhe_Planta));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.picture_detalhe_planta = new System.Windows.Forms.PictureBox();
+            this.lblNomePlanta = new System.Windows.Forms.Label();
+            this.lblNomeCientifico = new System.Windows.Forms.Label();
+            this.lblDetalhePlanta = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_detalhe_planta)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.lblDetalhePlanta);
+            this.panel1.Controls.Add(this.lblNomeCientifico);
+            this.panel1.Controls.Add(this.lblNomePlanta);
             this.panel1.Controls.Add(this.textBox3);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.picture_detalhe_planta);
             this.panel1.Location = new System.Drawing.Point(1, -5);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 456);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(302, 69);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(192, 196);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(200, 301);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(123, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(200, 338);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(123, 20);
-            this.textBox2.TabIndex = 2;
-            // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(461, 338);
+            this.textBox3.Location = new System.Drawing.Point(332, 326);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(123, 20);
             this.textBox3.TabIndex = 3;
             // 
-            // textBox4
+            // textBox2
             // 
-            this.textBox4.Location = new System.Drawing.Point(461, 301);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(123, 20);
-            this.textBox4.TabIndex = 4;
+            this.textBox2.Location = new System.Drawing.Point(332, 379);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(332, 272);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // picture_detalhe_planta
+            // 
+            this.picture_detalhe_planta.Location = new System.Drawing.Point(313, 55);
+            this.picture_detalhe_planta.Name = "picture_detalhe_planta";
+            this.picture_detalhe_planta.Size = new System.Drawing.Size(167, 162);
+            this.picture_detalhe_planta.TabIndex = 0;
+            this.picture_detalhe_planta.TabStop = false;
+            // 
+            // lblNomePlanta
+            // 
+            this.lblNomePlanta.AutoSize = true;
+            this.lblNomePlanta.Location = new System.Drawing.Point(335, 254);
+            this.lblNomePlanta.Name = "lblNomePlanta";
+            this.lblNomePlanta.Size = new System.Drawing.Size(35, 13);
+            this.lblNomePlanta.TabIndex = 4;
+            this.lblNomePlanta.Text = "Nome";
+            // 
+            // lblNomeCientifico
+            // 
+            this.lblNomeCientifico.AutoSize = true;
+            this.lblNomeCientifico.Location = new System.Drawing.Point(335, 309);
+            this.lblNomeCientifico.Name = "lblNomeCientifico";
+            this.lblNomeCientifico.Size = new System.Drawing.Size(83, 13);
+            this.lblNomeCientifico.TabIndex = 5;
+            this.lblNomeCientifico.Text = "Nome Científico";
+            // 
+            // lblDetalhePlanta
+            // 
+            this.lblDetalhePlanta.AutoSize = true;
+            this.lblDetalhePlanta.Location = new System.Drawing.Point(337, 362);
+            this.lblDetalhePlanta.Name = "lblDetalhePlanta";
+            this.lblDetalhePlanta.Size = new System.Drawing.Size(44, 13);
+            this.lblDetalhePlanta.TabIndex = 6;
+            this.lblDetalhePlanta.Text = "Detalhe";
             // 
             // Detalhe_Planta
             // 
@@ -99,7 +123,7 @@
             this.Text = "Detalhe_Planta";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_detalhe_planta)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -107,10 +131,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picture_detalhe_planta;
+        private System.Windows.Forms.Label lblDetalhePlanta;
+        private System.Windows.Forms.Label lblNomeCientifico;
+        private System.Windows.Forms.Label lblNomePlanta;
     }
 }
